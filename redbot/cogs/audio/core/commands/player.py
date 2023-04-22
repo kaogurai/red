df1281680d7e19ce073df0526bedbd316538215f
Ryan Fahey <git@ryan.science>
2023-04-22 13:35:54 -0400

    fix

--- redbot/cogs/audio/core/commands/player.py
+++ redbot/cogs/audio/core/commands/player.py
@@ -767,7 +767,7 @@ class PlayerCommands(MixinMeta, metaclass=CompositeMetaClass):
         if not self._player_check(ctx):
             if self.lavalink_connection_aborted:
                 msg = "Connection to Lavalink has failed"
-                desc = EmptyEmbed
+                desc = None
                 if await self.bot.is_owner(ctx.author):
                     desc = "Please check your console or logs for details."
                 return await self.send_embed_msg(ctx, title=msg, description=desc)
